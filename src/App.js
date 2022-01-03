9b62084a60e63d0e3f60a6c05550d2b39780dcc3
crowBottom <jpcunningham_dev@protonmail.com>
2022-01-02 23:24:09 -0600

    First commit for snack-or-booze files

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Home from "./Home";
+import SnackOrBoozeApi from "./Api";
+import NavBar from "./NavBar";
+import { Route, Switch } from "react-router-dom";
+import FoodMenu from "./FoodMenu";
+import DrinkMenu from "./DrinkMenu";
+import Snack from "./FoodItem";
+import Drink from "./DrinkItem";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [snacks, setSnacks] = useState([]);
+  const [drinks, setDrinks] = useState([]);
+
+  useEffect(() => {
+    async function getSnacks() {
+      let snacks = await SnackOrBoozeApi.getSnacks();
+      setSnacks(snacks);
+      setIsLoading(false);
+    }
+
+    async function getDrinks() {
+      let drinks = await SnackOrBoozeApi.getDrinks();
+      setDrinks(drinks);
+      setIsLoading(false);
+    }
+
+    getSnacks();
+    getDrinks();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading &hellip;</p>;
+  }
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+        <main>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/snacks">
+              <FoodMenu snacks={snacks} title="Snacks" />
+            </Route>
+            <Route path="/snacks/:id">
+              <Snack items={snacks} cantFind="/snacks" />
+            </Route>
+            <Route exact path="/drinks">
+              <DrinkMenu drinks={drinks} title="Drinks" />
+            </Route>
+            <Route path="/drinks/:id">
+              <Drink items={drinks} cantFind="/drinks" />
+            </Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
